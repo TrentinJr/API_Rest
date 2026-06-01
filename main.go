d714365511d2af71cb9875ccf543262506b4ad3c
TrentinJr <joaoluiztrentin@gmail.com>
2026-06-01 03:31:08 -0300

    code: remove comentarios do arquivo main

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// ===== PASSO 2: O Modelo de Dados =====
 type Tarefa struct {
 	gorm.Model
 	Titulo    string `json:"titulo"`
@@ -18,7 +17,6 @@ type Tarefa struct {
 	Concluida bool   `json:"concluida"`
 }
 
-// ===== PASSO 3: Variável Global e Conexão =====
 var DB *gorm.DB
 
 func ConectarBancoDeDados() {
@@ -34,9 +32,6 @@ func ConectarBancoDeDados() {
 	fmt.Println("Banco de dados conectado e tabelas criadas com sucesso!")
 }
 
-// ===== PASSO 4: Controladores (Ações da API) =====
-
-// 1. Criar uma nova tarefa
 func CriarTarefa(c *gin.Context) {
 	var novaTarefa Tarefa
 
@@ -49,14 +44,12 @@ func CriarTarefa(c *gin.Context) {
 	c.JSON(http.StatusCreated, novaTarefa)
 }
 
-// 2. Listar todas as tarefas
 func ListarTarefas(c *gin.Context) {
 	var tarefas []Tarefa
 	DB.Find(&tarefas)
 	c.JSON(http.StatusOK, tarefas)
 }
 
-// 3. Deletar uma tarefa por ID
 func DeletarTarefa(c *gin.Context) {
 	id := c.Param("id")
 	var tarefa Tarefa
@@ -70,7 +63,6 @@ func DeletarTarefa(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Tarefa deletada com sucesso!"})
 }
 
-// 4. PATCH: Altera APENAS se a tarefa está concluída ou não
 func AtualizarStatusTarefa(c *gin.Context) {
 	id := c.Param("id")
 	var tarefa Tarefa
@@ -93,7 +85,6 @@ func AtualizarStatusTarefa(c *gin.Context) {
 	c.JSON(http.StatusOK, tarefa)
 }
 
-// 5. PUT: Altera o texto completo da tarefa (Título e Descrição)
 func EditarTarefa(c *gin.Context) {
 	id := c.Param("id")
 	var tarefa Tarefa
@@ -112,13 +103,11 @@ func EditarTarefa(c *gin.Context) {
 	c.JSON(http.StatusOK, tarefa)
 }
 
-// ===== PASSO 5: Função Principal =====
 func main() {
 	ConectarBancoDeDados()
 
 	r := gin.Default()
 
-	// Registro de todas as rotas com o Gin
 	r.POST("/tarefas", CriarTarefa)
 	r.GET("/tarefas", ListarTarefas)
 	r.DELETE("/tarefas/:id", DeletarTarefa)
